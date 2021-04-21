3d70812c3dfa7b4aad71f35f0214f0a9767c8aea
MinJerry1129 <jinxiancheng1129@hotmail.com>
2021-04-22 07:58:01 +0800

    Test

--- app/src/main/java/com/mobiledevteam/imagepickertask/MainActivity.java
+++ app/src/main/java/com/mobiledevteam/imagepickertask/MainActivity.java
@@ -5,23 +5,31 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.DefaultSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
 import com.github.dhaval2404.imagepicker.ImagePicker;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.smarteist.autoimageslider.SliderView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    SliderView _slideImage;
+    private SliderLayout sliderLayout;
+    private List<String> imageUrls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +37,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        _slideImage = findViewById(R.id.slider_image);
+        sliderLayout = findViewById(R.id.imageSlider);
+        imageUrls=new ArrayList<>();
+//        setSliderViews_main();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -51,9 +61,38 @@ public class MainActivity extends AppCompatActivity {
 //            _txtSelImage.setVisibility(View.GONE);
 //            _imgClinic.setImageURI(fileUri);
 //            File file = ImagePicker.Companion.getFile(data);
-//            filePath = ImagePicker.Companion.getFilePath(data);
+            String filePath = ImagePicker.Companion.getFilePath(data);
+            imageUrls.add(filePath);
+            setSliderViews();
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+    private void setSliderViews() {
+        this.runOnUiThread(new Runnable() {
+            public void run() {
+                sliderLayout.removeAllSliders();
+//        sliderLayout.removeAllViews();
+                for (String theImageUrl: imageUrls) {
+//            TextSliderView textSliderView = new TextSliderView(this);
+//            textSliderView.image(theImageUrl).setScaleType(BaseSliderView.ScaleType.CenterCrop);
+//            sliderLayout.addSlider(textSliderView);
+                    File imgFile = new File(theImageUrl);
+                    DefaultSliderView sliderView = new DefaultSliderView(getBaseContext());
+                    sliderView.image(imgFile).setScaleType(BaseSliderView.ScaleType.CenterCrop);
+                    sliderLayout.addSlider(sliderView);
+                    Log.d("Filepath:", theImageUrl);
+                }
+            }
+        });
+    }
+    public void setSliderViews_main() {
+        for (int i=0;i<5;i++) {
+            String theImageUrl="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png";
+//            imageUrls.add(theImageUrl);
+            TextSliderView textSliderView = new TextSliderView(this);
+            textSliderView.image(theImageUrl).setScaleType(BaseSliderView.ScaleType.CenterCrop);
+            sliderLayout.addSlider(textSliderView);
+        }
     }
 
     @Override
